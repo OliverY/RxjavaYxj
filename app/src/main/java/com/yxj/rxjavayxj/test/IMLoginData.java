b82d199cf13b21f14a0e5bbe177bf15c8762d679
yanxujun <yanxujun@yuantutech.com>
2019-03-22 15:17:17 +0800

    临时用

---
+++ app/src/main/java/com/yxj/rxjavayxj/test/IMLoginData.java
@@ -0,0 +1,16 @@
+package com.yxj.rxjavayxj.test;
+
+/**
+ * Author:  Yxj
+ * Time:    2019/3/22 下午1:54
+ * -----------------------------------------
+ * Description:
+ */
+public class IMLoginData {
+
+    public boolean isSuccess;
+
+    public IMLoginData(boolean isSuccess) {
+        this.isSuccess = isSuccess;
+    }
+}
---
+++ app/src/main/java/com/yxj/rxjavayxj/test/LocationData.java
@@ -0,0 +1,18 @@
+package com.yxj.rxjavayxj.test;
+
+/**
+ * Author:  Yxj
+ * Time:    2019/3/22 下午1:45
+ * -----------------------------------------
+ * Description:
+ */
+public class LocationData {
+
+    public String city;
+    public String unionId;
+
+    public LocationData(String city, String unionId) {
+        this.city = city;
+        this.unionId = unionId;
+    }
+}
